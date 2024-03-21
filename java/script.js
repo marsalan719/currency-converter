78c041366fa850995744d9503af32ace41458ab8
Muhammad Arsalan <161702988+marsalan719@users.noreply.github.com>
2024-03-22 02:03:40 +0500

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,210 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+const BASE_URL =
+  "https://v6.exchangerate-api.com/v6/53577c2c0cc6e7e38ae28a1f/latest/";
+
+let selects = document.querySelectorAll(".dropdown");
+let drp1 = document.querySelector(".dropdown1");
+let img1 = document.querySelector(".img1");
+let drp2 = document.querySelector(".dropdown2");
+let img2 = document.querySelector(".img2");
+let amount = document.querySelector(".amount");
+let result = document.querySelector(".result");
+let btn = document.querySelector(".btn");
+let resultval = document.querySelector(".resultval");
+for (let select of selects) {
+  for (country in countryList) {
+    let newoption = document.createElement("option");
+    newoption.innerText = country;
+    newoption.value = country;
+    newoption.classList.add("option");
+    select.append(newoption);
+    if (select.name === "from" && newoption.value === "PKR") {
+      newoption.selected = "selected";
+    } else if (select.name === "to" && newoption.value === "USD") {
+      newoption.selected = "selected";
+    }
+  }
+}
+const getex = async () => {
+  const url = `${BASE_URL}/${drp1.value.toLowerCase()}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  let rate = data.conversion_rates[drp2.value];
+  let total = rate * amount.value;
+  resultval.innerText = `${amount.value} ${drp1.value}=${total} ${drp2.value}`;
+};
+
+drp1.addEventListener("change", () => {
+  let cuncode = countryList[drp1.value];
+  img1.innerHTML = `<img src="https://flagsapi.com/${cuncode}/flat/64.png"/>`;
+  getex();
+});
+drp2.addEventListener("change", () => {
+  let cuncode = countryList[drp2.value];
+  img2.innerHTML = `<img src="https://flagsapi.com/${cuncode}/flat/64.png"/>`;
+  getex();
+});
+
+getex();
+
+btn.addEventListener("click", getex);
